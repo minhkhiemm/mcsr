2faccf6010aa480425e86303720f41f8a0aff7cd
minhkhiemm <vominhkhiem212@gmail.com>
2020-12-04 21:45:24 +0700

    feat: add parse args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 use dotenv::dotenv;
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
@@ -7,16 +8,43 @@ use std::env;
 
 fn main() {
     dotenv().ok();
-    env_logger::init();
+    pretty_env_logger::init();
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("address")
+                .short("a")
+                .long("address")
+                .value_name("ADDRESS")
+                .help("Sets an address")
+                .takes_value(true),
+        )
+        .get_matches();
     info!("Rand Microservice - v0.1.0");
     trace!("Starting...");
-    let addr = env::var("ADDRESS")
-        .unwrap_or_else(|_| "127.0.0.1:8080".into())
+
+    let addr = matches
+        .value_of("address")
+        .map(|s| s.to_owned())
+        .or_else(|| env::var("ADDRESS").ok())
+        .unwrap_or_else(|| "127.0.0.1:8000".into())
         .parse()
-        .expect("can't parse ADDRESS variable");
+        .expect("can;t parse ADDRESS variable");
     debug!("Trying to bind server to address: {}", addr);
+
     let builder = Server::bind(&addr);
     trace!("Creating service handler...");
+
     let server = builder.serve(|| {
         service_fn_ok(|req| {
             trace!("Incoming request is: {:?}", req);
@@ -26,6 +54,7 @@ fn main() {
         })
     });
     info!("Used address: {}", server.local_addr());
+
     let server = server.map_err(drop);
     debug!("Run!");
     hyper::rt::run(server);
