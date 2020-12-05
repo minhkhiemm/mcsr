0616e05b1458a4c70c4cec34b1420a869229afed
minhkhiemm <vominhkhiem212@gmail.com>
2020-12-05 11:15:09 +0700

    feat: add random routes

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,86 @@
-use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
-use dotenv::dotenv;
-use hyper::rt::Future;
-use hyper::service::service_fn_ok;
-use hyper::{Body, Response, Server};
-use log::{debug, info, trace};
-use std::env;
+extern crate futures;
+extern crate hyper;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+use futures::{future, Future, Stream};
+use hyper::service::service_fn;
+use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
+use rand::distributions::{Bernoulli, Normal, Uniform};
+use rand::Rng;
+use std::ops::Range;
+
+static INDEX: &[u8] = b"Random Microservice";
+
+#[derive(Serialize)]
+struct RngResponse {
+    value: f64,
+}
+
+#[derive(Deserialize)]
+enum RngRequest {
+    Uniform {
+        #[serde(flatten)]
+        range: Range<i32>,
+    },
+    Normal {
+        mean: f64,
+        std_dev: f64,
+    },
+    Bernoulli {
+        p: f64,
+    },
+}
+
+fn handle_request(request: RngRequest) -> RngResponse {
+    let mut rng = rand::thread_rng();
+    let value = {
+        match request {
+            RngRequest::Uniform { range } => rng.sample(Uniform::from(range)) as f64,
+            RngRequest::Normal { mean, std_dev } => rng.sample(Normal::new(mean, std_dev)) as f64,
+            RngRequest::Bernoulli { p } => rng.sample(Bernoulli::new(p)) as i8 as f64,
+        }
+    };
+    RngResponse { value }
+}
+
+fn microservice_handler(
+    req: Request<Body>,
+) -> Box<dyn Future<Item = Response<Body>, Error = Error> + Send> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") | (&Method::GET, "/random") => {
+            Box::new(future::ok(Response::new(INDEX.into())))
+        }
+        (&Method::POST, "/random") => {
+            let body = req.into_body().concat2().map(|chunks| {
+                let res = serde_json::from_slice::<RngRequest>(chunks.as_ref())
+                    .map(handle_request)
+                    .and_then(|resp| serde_json::to_string(&resp));
+                match res {
+                    Ok(body) => Response::new(body.into()),
+                    Err(err) => Response::builder()
+                        .status(StatusCode::UNPROCESSABLE_ENTITY)
+                        .body(err.to_string().into())
+                        .unwrap(),
+                }
+            });
+            Box::new(body)
+        }
+        _ => {
+            let resp = Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body("Not Found".into())
+                .unwrap();
+            Box::new(future::ok(resp))
+        }
+    }
+}
 
 fn main() {
-    dotenv().ok();
-    pretty_env_logger::init();
-    let matches = App::new(crate_name!())
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .arg(
-            Arg::with_name("config")
-                .short("c")
-                .long("config")
-                .value_name("FILE")
-                .help("Sets a custom config file")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("address")
-                .short("a")
-                .long("address")
-                .value_name("ADDRESS")
-                .help("Sets an address")
-                .takes_value(true),
-        )
-        .get_matches();
-    info!("Rand Microservice - v0.1.0");
-    trace!("Starting...");
-
-    let addr = matches
-        .value_of("address")
-        .map(|s| s.to_owned())
-        .or_else(|| env::var("ADDRESS").ok())
-        .unwrap_or_else(|| "127.0.0.1:8000".into())
-        .parse()
-        .expect("can;t parse ADDRESS variable");
-    debug!("Trying to bind server to address: {}", addr);
-
+    let addr = ([127, 0, 0, 1], 8080).into();
     let builder = Server::bind(&addr);
-    trace!("Creating service handler...");
-
-    let server = builder.serve(|| {
-        service_fn_ok(|req| {
-            trace!("Incoming request is: {:?}", req);
-            let random_byte = rand::random::<u8>();
-            debug!("Generated value is: {}", random_byte);
-            Response::new(Body::from(random_byte.to_string()))
-        })
-    });
-    info!("Used address: {}", server.local_addr());
-
+    let server = builder.serve(|| service_fn(microservice_handler));
     let server = server.map_err(drop);
-    debug!("Run!");
     hyper::rt::run(server);
 }
